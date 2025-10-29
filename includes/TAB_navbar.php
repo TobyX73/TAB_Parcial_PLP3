7c670423c850c08aba173e110d6ec88358458aed
Toby <tobybecerra04@gmail.com>
2025-10-29 19:07:17 -0300

    Build: Agregue las primeras carpetas para sentar la base del proyecto.

---
+++ includes/TAB_navbar.php
@@ -0,0 +1,5 @@
+<nav>
+    <ul>
+        <li><a href="/TAB_PARCIAL_PLP3/">Ver menu</a></li>
+    </ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Mamelodies Fast Food</title>
+    <link rel="stylesheet" href="css/TAB_estilos.css">
+</head>
+<body>
+    <?php include 'include/navbar.php'; ?>
+    <header>
+        <h1>Concurso de disfraces de Halloween</h1>
+    </header>
+    <main>
+        <section id="comidas-list" class="section">
+            <!-- Aquí se mostrarán los comidas -->
+            
+            <div class="comida">
+                <h2>Hamburguesa</h2>
+                <p>Contiene tomate, lechuga, carne, huevo y cebolla</p>
+                <p><img src="imagenes/fondo.jpg" width="100%"></p>
+                <button class="Agregar">Agregar al Carrito</button>
+            </div>
+            <hr>
+            <div class="comida">
+                <h2>Pizza muzzarella</h2>
+                <p>Contiene muzarella, oregano y salsa de tomate</p>
+                <p><img src="imagenes/fondo.jpg" width="100%"></p>
+                <button class="Agregar">Agregar al Carrito</button>
+            </div>
+            <hr>
+            <div class="comida">
+                <h2>Ensalada César</h2>
+                <p>Contiene lechuga, pollo, croutones y salsa césar</p>
+                <p><img src="imagenes/fondo.jpg" width="100%"></p>
+                <button class="Agregar">Agregar al Carrito</button>
+            </div>
+            <hr>
+            
+        
+        </section>
+    </main>
+    <script src="js/TAB_script.js"></script>
+</body>
+</html>
